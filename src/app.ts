f3378f06d6231c52de4e7acc0ac0f0afd4473632
Abolfazl Alizadeh <mrabolfazlalz@gmail.com>
2021-10-15 16:41:47 +0330

    Update environments methods

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,50 @@
-import express = require('express');
-import {sequelize} from './config/database';
-import Routes from './routes/routes';
-import FileService from './services/file.services';
-import cors = require('cors');
+import express = require('express')
+import { sequelize } from './config/database'
+import Routes from './routes/routes'
+import FileService from './services/file.services'
+import cors = require('cors')
 
-require('./config/config');
-require('./config/lang');
+require('./config/config')
+require('./config/lang')
 
-const app = express();
-// app.listen
-app.use(express.static(FileService.public.path));
-app.use((express.urlencoded({extended: true})));
-app.use(express.json());
-app.use(express.raw());
-app.use(cors());
+const app = express()
+app.use(express.static(FileService.public.path))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.raw())
+app.use(cors())
 
-const port = Number.parseInt(process.env.SERVER_PORT ?? '8000') ?? 8000;
-const hostname = process.env.SERVER_HOST ?? 'localhost';
+const port = Number.parseInt(getArgsValue('port', 'SERVER_PORT', '8000'))
+const hostname = getArgsValue('host', 'SERVER_HOST', 'localhost')
 
-Routes.init(app);
+Routes.init(app)
 
 export const server = app.listen(port, hostname, () => {
-  connectDatabase(() => {
-    console.log('Server started');
-    console.log(`Server listening at http://${hostname}:${port}`);
-  });
-});
+    connectDatabase(() => {
+        console.log('Server started')
+        console.log(`Server listening at http://${hostname}:${port}`)
+    })
+})
 
 const connectDatabase = (listener: CallableFunction) => {
-  sequelize
-      .authenticate()
-      .then(async () => {
-        console.log('database connected');
-        listener();
-      })
-      .catch((e: Error) => {
-        console.error('Database Authenticate Error:', e.message);
-      });
-};
+    sequelize
+        .authenticate()
+        .then(async () => {
+            console.log('database connected')
+            listener()
+        })
+        .catch((e: Error) => {
+            console.error('Database Authenticate Error:', e.message)
+        })
+}
 
-export default app;
+function getArgsValue(argName: string, envKey: string, defaultValue: string) {
+    const args = process.argv
+    if (args.includes(argName)) {
+        const index = args.indexOf(argName)
+        if (args.length > index + 1) return args[index + 1] ?? defaultValue
+    }
+    return process.env[envKey] ?? defaultValue
+}
+
+export default app
