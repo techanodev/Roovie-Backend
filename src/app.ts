2046db6ade7c2bc7052cbc624548428ad46a28c6
Abolfazl Alizadeh <mrabolfazlalz@gmail.com>
2021-10-15 10:20:29 +0330

    Fix running async

--- src/app.ts
+++ src/app.ts
@@ -1,41 +1,42 @@
-import express = require('express')
-import { sequelize } from './config/database'
-import Routes from './routes/routes'
-import FileService from './services/file.services'
-import cors = require('cors')
+import express = require('express');
+import {sequelize} from './config/database';
+import Routes from './routes/routes';
+import FileService from './services/file.services';
+import cors = require('cors');
 
-require('./config/config')
-require('./config/lang')
+require('./config/config');
+require('./config/lang');
 
-const app = express()
-app.use(express.static(FileService.public.path))
-app.use((express.urlencoded({ extended: true })))
-app.use(express.json())
-app.use(express.raw())
-app.use(cors())
+const app = express();
+// app.listen
+app.use(express.static(FileService.public.path));
+app.use((express.urlencoded({extended: true})));
+app.use(express.json());
+app.use(express.raw());
+app.use(cors());
 
-const port = Number.parseInt(process.env.SERVER_PORT ?? '8000') ?? 8000
-const hostname = process.env.SERVER_HOST ?? 'localhost'
+const port = Number.parseInt(process.env.SERVER_PORT ?? '8000') ?? 8000;
+const hostname = process.env.SERVER_HOST ?? 'localhost';
 
-Routes.init(app)
+Routes.init(app);
 
-app.listen(port, hostname, () => {
-    connectDatabase(() => {
-        console.log('Server started')
-        console.log(`Server listening at http://${hostname}:${port}`)
-    })
-})
+export const server = app.listen(port, hostname, () => {
+  connectDatabase(() => {
+    console.log('Server started');
+    console.log(`Server listening at http://${hostname}:${port}`);
+  });
+});
 
-function connectDatabase(listener: CallableFunction) {
-    sequelize
-        .authenticate()
-        .then(async () => {
-            console.log('database connected')
-            listener()
-        })
-        .catch((e: Error) => {
-            console.error('Database Authenticate Error:', e.message)
-        })
-}
+const connectDatabase = (listener: CallableFunction) => {
+  sequelize
+      .authenticate()
+      .then(async () => {
+        console.log('database connected');
+        listener();
+      })
+      .catch((e: Error) => {
+        console.error('Database Authenticate Error:', e.message);
+      });
+};
 
-export default app
+export default app;
--- src/test/routes/users/auth.test.ts
+++ src/test/routes/users/auth.test.ts
@@ -1,30 +1,1 @@
-import { PhoneNumber } from '../../../models/users/users.models'
-import app from '../../../app'
-import request = require('supertest')
-import faker = require('faker')
-import UserCrud from '../../../crud/users/users.crud'
-import crypto = require('crypto')
-
-beforeEach(() => {
-    process.env.NODE_ENV = 'dev'
-    process.env.JWT_SECRET = crypto.randomBytes(256).toString('base64')
-})
-
-describe('Test create account requests -> ', () => {
-    it('It should response with 200 status code', async () => {
-        const phone: PhoneNumber = {
-            number: faker.phone.phoneNumber('9xxxxxxxxx'),
-        }
-        let code = ''
-        try {
-            code = await UserCrud.sendValidationCode(phone, 'create_account')
-        } catch {}
-        request(app)
-            .post('/v1/account/auth/login')
-            .send({
-                phone_number: phone.number,
-                code: code,
-            })
-            .expect(200)
-    })
-})
+import {PhoneNumber} from '../../../models/users/users.models';import app, {server} from '../../../app';import UserCrud from '../../../crud/users/users.crud';import request = require('supertest');import faker = require('faker');import crypto = require('crypto');beforeEach(() => {  process.env.NODE_ENV = 'dev';  process.env.JWT_SECRET = crypto.randomBytes(256).toString('base64');});afterAll(() => {  server.close();});describe('Test create account requests -> ', () => {  process.env.NODE_ENV = 'dev';  it('It should response with 200 status code', async () => {    const phone: PhoneNumber = {      number: faker.phone.phoneNumber('9xxxxxxxxx'),    };    let code = '';    try {      code = await UserCrud.sendValidationCode(phone, 'create_account');    } catch {    }    request(app)        .post('/v1/account/auth/login')        .send({          phone_number: phone.number,          code: code,        })        .expect(200);  });});
